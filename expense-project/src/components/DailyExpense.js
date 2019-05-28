073b779469832028cd2e876c6922cdcdabb33d3a
Witthaya Luanghirun <minimalwit@gmail.com>
2019-05-28 23:20:43 +0700

    Wit: replace expense mode with switch button

--- expense-project/src/components/DailyExpense.js
+++ expense-project/src/components/DailyExpense.js
@@ -1,98 +1,130 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import '../css/expense.css'
 import { connect } from 'react-redux'
 // import DatePicker from 'react-date-picker';
 
-import ExpenseList from './ExpenseList';
+import ExpenseList from './ExpenseList'
 class DailyExpense extends Component {
-    state = {
-        amount: 0,
-        remark: '',
-        type: 'expense',
-    }
-    componentDidMount() {
+  state = {
+    amount: 0,
+    remark: '',
+    type: 'expense',
+  }
 
-    }
+  // componentWillUpdate(prevProps,prevState){
+  //   if(prevState !== this.state){
+  //     this.setState(this.state)
+  //   }
+  // }
 
-    submitAdd = (e) => {
-        e.preventDefault();
-        this.props.createTransaction({date : this.props.date, user_id: localStorage.getItem('token'), amount: this.state.amount, remark: this.state.remark, type: this.state.type  })
-    }
+  componentDidMount() {}
 
-    handleChange = (e) => {
-        this.setState({ ...this.state, [e.target.name]: e.target.value })
-    }
+  submitAdd = (e) => {
+    e.preventDefault()
+    this.props.createTransaction({
+      date: this.props.date,
+      user_id: localStorage.getItem('token'),
+      amount: this.state.amount,
+      remark: this.state.remark,
+      type: this.state.type,
+    })
+  }
 
-    render() {
-        console.log(this.props.date)
-        return (
-            <div className="box-daily-expense">
-                <div className="header-daily-expense">
-                    <div className="label">
-                        Expense Daily
-                    </div>
-                    <div>
-                        <button className="add-expense-btn" onClick={this.show}>
-                            <span className="icon-plus"><i className="fas fa-plus"></i></span> add
-                        </button>
-                    </div>
+  handleChange = (e) => {
+    this.setState({ ...this.state, [e.target.name]: e.target.value })
+  }
+
+  render() {
+    console.log(this.props.date)
+    return (
+      <div className="box-daily-expense">
+        <div className="header-daily-expense">
+          <div className="label">Expense Daily</div>
+          <div>
+            <button className="add-expense-btn" onClick={this.show}>
+              <span className="icon-plus">
+                <i className="fas fa-plus" />
+              </span>{' '}
+              add
+            </button>
+          </div>
+        </div>
+        <div className="from-add-expense">
+          <form onSubmit={this.submitAdd}>
+            <div className="form-add-exp-wrapper">
+              <div className="form-left">
+                <div className="expense-mode-btn-wrapper">
+                  <input
+                    type="button"
+                    className={ (this.state.type === 'expense') ? 'exp-button-active' : 'exp-button-inactive' }
+                    style={{
+                      width: '25%',
+                      border: 'none',
+                    }}
+                    name="type"
+                    value="expense"
+                    onClick={(e) => this.handleChange(e)}
+                  />
+                  <input
+                    type="button"
+                    className={ (this.state.type === 'income') ? 'exp-button-active' : 'exp-button-inactive' }
+                    style={{
+                      width: '25%',
+                      border: 'none',
+                    }}
+                    name="type"
+                    value="income"
+                    onClick={(e) => this.handleChange(e)}
+                  />
                 </div>
-                <div className="from-add-expense">
-                    <form onSubmit={this.submitAdd}>
-                        <div className="form-add-exp-wrapper">
-                            <div className="form-left">
-                                <div>
-                                    amount
-                            <input
-                                        type="number"
-                                        value={this.state.amount}
-                                        name="amount"
-                                        onChange={this.handleChange}>
-                                    </input>
-                                </div>
 
-                                <div>
-                                    Remark
-                            <input
-                                        type="text"
-                                        name="remark"
-                                        value={this.state.remark}
-                                        onChange={this.handleChange}
-                                    ></input>
-                                </div>
-
-
-                                <select
-                                    name="type"
-                                    value={this.state.type}
-                                    onChange={this.handleChange}>
-                                    <option value="expense">Expense</option>
-                                    <option value="income">Income</option>
-                                </select>
-                            </div>
-
-                            <div><button type="submit"> เพิ่มเข้าสู่ประวัติ </button></div>
-                        </div>
-                    </form>
+                <div>
+                  amount
+                  <input
+                    type="number"
+                    value={this.state.amount}
+                    name="amount"
+                    onChange={this.handleChange}
+                  />
                 </div>
-                <div className="expense-list-wrapper">
-                    <ExpenseList date={this.props.date}/>
+
+                <div>
+                  Remark
+                  <input
+                    type="text"
+                    name="remark"
+                    value={this.state.remark}
+                    onChange={this.handleChange}
+                  />
                 </div>
+
+              </div>
+
+              <div>
+                <button type="submit"> Submit </button>
+              </div>
             </div>
-        );
-    }
+          </form>
+        </div>
+        <div className="expense-list-wrapper">
+          <ExpenseList date={this.props.date} />
+        </div>
+      </div>
+    )
+  }
 }
 
-
 const mapStateToProps = (state) => ({
-    exp: state.exp
+  exp: state.exp,
 })
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        createTransaction: dispatch.exp.createTransaction
-    }
+  return {
+    createTransaction: dispatch.exp.createTransaction,
+  }
 }
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(DailyExpense);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DailyExpense)
